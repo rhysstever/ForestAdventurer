9612fc0730b849ee019ebc6f9edd8d7aa080e321
Rhys Stever <rhys@stever.com>
2025-11-05 16:47:07 -0500

    Units now display health and defense values

--- Assets/Scripts/Unit Scripts/Unit.cs
+++ Assets/Scripts/Unit Scripts/Unit.cs
@@ -1,8 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
     // Instantiated in inspector
+    [SerializeField]
+    private TMP_Text lifeText, defenseText;
     [SerializeField]
     protected int maxLife;
 
@@ -19,15 +22,25 @@ public class Unit : MonoBehaviour
 
         // Damage is dealt to defense before health
         if(currentDefense > 0) {
+            // If the unit has defense, account for damaging the defense first
             if(amount > currentDefense) {
-                currentLife -=
+                // If the damage dealt is more than the unit's defense,
+                // subtract the difference from the unit's health
+                currentLife -= (amount - currentDefense);
                 currentDefense = 0;
             } else {
+                // If the damage dealth is less than the unit's defense,
+                // subtract it from the current defense
                 currentDefense -= amount;
             }
+            // Update defense UI text
+            UpdateDefenseUIText();
         } else {
+            // If the unit has no defense, subtract the damage from its health
             currentLife -= amount;
         }
+        // Update life UI text
+        UpdateLifeUIText();
     }
 
     public void Heal(int amount) {
@@ -39,6 +52,8 @@ public class Unit : MonoBehaviour
         if(currentLife > maxLife) {
             currentLife = maxLife;
         }
+        // Update life UI text
+        UpdateLifeUIText();
     }
 
     public void GiveDefense(int amount) {
@@ -47,14 +62,27 @@ public class Unit : MonoBehaviour
         }
 
         currentDefense += amount;
+        UpdateDefenseUIText();
     }
 
     public void ClearDefense() {
         currentDefense = 0;
+        UpdateDefenseUIText();
+    }
+
+    protected void UpdateLifeUIText() {
+        lifeText.text = string.Format("{0}/{1}", currentLife, maxLife);
+    }
+
+    protected void UpdateDefenseUIText() {
+        defenseText.text = currentDefense.ToString();
     }
 
     public virtual void Reset() {
         currentLife = maxLife; 
         currentDefense = 0;
+        // Update both UI text
+        UpdateLifeUIText();
+        UpdateDefenseUIText();
     }
 }
